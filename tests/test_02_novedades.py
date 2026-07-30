cbdc26e7d1ea271d1511883c6957d5e9c22509aa
iker <iker.rivera@servitel.co>
2026-07-30 13:53:44 +0000

    logs novedades

--- tests/test_02_novedades.py
+++ tests/test_02_novedades.py
@@ -139,12 +139,14 @@ def test_btt_crear_novedad(driver_logueado):
             MENU_NOVEDADES
         ).click()
 
+        logger.info("Abriendo crear novedad")
         WebDriverWait(driver_logueado, 10).until(
             EC.element_to_be_clickable(
                 (By.XPATH, BOTON_CREAR_NOVEDAD)
             )
         ).click()
 
+        logger.info("Esperando que aparezca el formulario de novedad")
         WebDriverWait(driver_logueado, 20).until(
             EC.visibility_of_element_located(
                 (By.XPATH, "//*[contains(text(),'Registrar novedad')]")
@@ -170,82 +172,91 @@ def test_btt_crear_novedad(driver_logueado):
 def test_btones_novedades(driver_logueado):
     try:
 
+        logger.info("========== INICIO TEST_BTONES_NOVEDADES ==========")
         driver_logueado.find_element(
             By.XPATH,
             SIDEBAR_BOTON
         ).click()
 
-        # Abrir módulo de novedades
+        logger.info("Abriendo módulo de novedades")
         driver_logueado.find_element(
             By.XPATH,
             MENU_NOVEDADES
         ).click()
 
+        logger.info("Abriendo ver novedades")
         driver_logueado.find_element(
             By.XPATH,
             BOTON_VER_NOVEDADES
         ).click()
 
-        # Guardar la pestaña principal
+        logger.info("guardando la pestaña principal")
         pestana_principal = driver_logueado.current_window_handle
 
-        # Clic en botón de previsualizar PDF
+        logger.info("Abriendo PDF de la tercera novedad")
         driver_logueado.find_element(
             By.XPATH,
             '//*[@id="tabla-mias"]/tbody/tr[3]/td[9]/div/a[1]'
         ).click()
 
-        # Esperar que aparezca la nueva pestaña
+        logger.info("Esperando que aparezca la nueva pestaña")
         WebDriverWait(driver_logueado, 10).until(
             lambda driver: len(driver.window_handles) > 1
         )
 
-        # Cambiar a la nueva pestaña (PDF)
+        logger.info("Cambiando a la nueva pestaña (PDF)")
         for pestana in driver_logueado.window_handles:
             if pestana != pestana_principal:
                 driver_logueado.switch_to.window(pestana)
                 break
 
+        logger.info("Esperando que el PDF cargue")
         # Aquí validas que el PDF abrió
         print("URL PDF:", driver_logueado.current_url)
 
+        logger.info("Validando que se abrieron dos pestañas")
         assert len(driver_logueado.window_handles) == 2
 
 
-        # Cerrar pestaña del PDF
+        logger.info("Cerrando la pestaña del PDF")
         driver_logueado.close()
 
-
-        # Volver a la pestaña principal
+        logger.info("Volviendo a la pestaña principal")
         driver_logueado.switch_to.window(pestana_principal)
 
+        logger.info("presionando el botón de editar de la tercera novedad")
         #editar
         driver_logueado.find_element(
             By.XPATH,
             '//*[@id="tabla-mias"]/tbody/tr[3]/td[9]/div/a[2]'
         ).click()
 
+        logger.info("Esperando que aparezca el formulario de edición")
         WebDriverWait(driver_logueado, 15).until(
             EC.url_contains("/edit")
         )
 
+        logger.info("Esperando que aparezca el mensaje de 'Editar novedad'")
         WebDriverWait(driver_logueado, 10).until(
             EC.presence_of_element_located(
                 (By.XPATH, "//*[contains(normalize-space(.), 'Editar novedad')]")
             )
         )
 
+        logger.info("Volviendo a la pestaña principal")
         driver_logueado.find_element(
             By.XPATH,
             '/html/body/div/div[2]/div/div/div/div/div[1]/a'
         ).click()
 
+        logger.info("presinando el botón de previsualizar de la tercera novedad")
         #previsualizar
         driver_logueado.find_element(
             By.XPATH,
             '//*[@id="tabla-mias"]/tbody/tr[3]/td[9]/div/button'
         ).click()
 
+        logger.info("Esperando que aparezca el modal de previsualización")
         WebDriverWait(driver_logueado, 15).until(
             EC.presence_of_element_located(
                 (By.XPATH, '//*[@id="modal-detalle"]/div/div')
